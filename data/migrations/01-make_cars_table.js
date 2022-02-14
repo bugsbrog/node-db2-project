6eaeef51e2f66deacd7b6f53f173635e923c2de2
bugsbrog <bugsbrog@gmail.com>
2022-02-14 16:49:14 -0700

    Got migration file set up

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {})
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars')
 };
