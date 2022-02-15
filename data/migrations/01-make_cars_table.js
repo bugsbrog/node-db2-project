a5310660b8587a430b0d631ceb5d567ed277859f
bugsbrog <bugsbrog@gmail.com>
2022-02-14 17:26:01 -0700

    Done with migration file

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,18 @@
 exports.up = function (knex) {
-  return knex.schema.createTable('cars', table => {})
+  return knex.schema.createTable('cars', table => {
+      table.increments()
+      table.string('vin', 17)
+          .unique()
+          .notNullable()
+      table.string('make', 128)
+          .notNullable()
+      table.string('model', 128)
+          .notNullable()
+      table.integer('mileage')
+          .notNullable()
+      table.string('title', 128).defaultTo('clean')
+      table.string('transmission', 128).defaultTo('automatic')
+  })
 };
 
 exports.down = function (knex) {
